22bd73de096cac45b9eb6f87740c708ec3a1c6ef
Diego Panqueva <daerb9011@gmail.com>
2023-11-09 16:02:08 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+
+import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './components/products/products.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { AlliancesComponent } from './components/alliances/alliances.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ProductsComponent,
+    CounterComponent,
+    ContactComponent,
+    AboutComponent,
+    AlliancesComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
