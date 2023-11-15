3e99b2d7ce92833eced0897520c28b0c27057a4d
Diego Panqueva <daerb9011@gmail.com>
2023-11-15 13:09:27 -0500

    valid form contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from 'src/app/core/model/contact';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
 
+  contact: Contact = new Contact();
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  contactMe():void{
+    console.log('contact' + this.contact.name);
+  }
 }
---
+++ src/app/core/model/contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+
+    name?: string;
+    email?: string;
+    cellphone?: number;
+    message?: string;
+
+}
