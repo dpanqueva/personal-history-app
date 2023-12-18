143c0f54efeaf7cca22016ca653a85cd225f07bd
Diego Panqueva <daerb9011@gmail.com>
2023-12-18 06:18:38 -0500

    change issue and fix comment and alert

--- src/app/common/env/environment.prod.ts
+++ src/app/common/env/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment ={
     mensaje_error: 'sweet-alert-error',
     mensaje_no_encontrado: 'sweet-alert-not-found',
     mensaje_internal_error: 'sweet-alert-internal-error',
-    contacto_registrado_ok: 'sweet-alert-regitered-contact',
+    contacto_registrado_ok: 'sweet-alert-registered-contact',
     recaptcha_key:"6LcqUS8pAAAAAIyGtEZavwjGUqW5n8jL4an47zFJ"
 
 }
