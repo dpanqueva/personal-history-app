a1299fc32c3b94b9055161366a22cb04aefc8d1b
dpanqueva <daerb9011@gmail.com>
2024-11-11 22:27:34 +0100

    Update map.component.ts

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+gimport { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Control, LayerGroup, Map, marker, tileLayer } from 'leaflet';
 import { Geocoder } from 'leaflet-control-geocoder';
@@ -79,7 +79,7 @@ export class MapComponent {
   });
 
   private async apiGoogle() {
-    const apikey = "AIzaSyBbCaPJDJSPN8Zon_cciGrw_RhkgCGYHr4";
+    const apikey = "_cciGrw_";
     this.httpHeaders.append('Authorization', 'Bearer ' + apikey);
     // Obtenemos la dirección del usuario
     const address = "CLL 60 A SUR # 73-31, Bogotá, Colombia";
